1f57920598b24b7c488d3ccf9d802f28fedaaa87
Stef-R <username@user.noreply.github.com>
2016-11-14 14:52:46 +0100

    add proj og start

---
+++ DemoDict/DemoDict/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 32;
+            List<int> evenNumber = new List<int>();
+            Dictionary<int,bool>  myNum= new Dictionary<int,bool>();
+            for (int i = 0; i <= z; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumber.Add(i);
+                }
+                else
+                {
+
+                }
+            }
+            foreach (var item in evenNumber)
+            {
+                //Console.WriteLine("Lige = {0}", item);
+            }
+
+            //////////////////////////////
+            MyNumber x = new MyNumber();
+            x.value = 3;
+
+            AddTo(x, 1);
+
+            Console.WriteLine("svar:",x );
+
+            Console.ReadKey();
+        }
+        static void AddTo(MyNumber x,int y)
+        {
+            x.value += y;
+        }
+    }
+    public class MyNumber
+    {
+        public int value;
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+
+}
