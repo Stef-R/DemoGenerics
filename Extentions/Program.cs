bfa0a5ab5a7a9871b8c87556fb3949fe7a172311
Stef-R <username@user.noreply.github.com>
2016-11-15 14:50:17 +0100

    Add extention swapgeeneric

---
+++ Extentions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extentions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 tal = 234;
+            Debug.WriteLine("tal={0} ",tal);
+            Debug.WriteLine(tal.Times1000());
+            Console.ReadKey();
+        }
+    }
+    public static class MyExtention
+    {
+        public static Int32 Times1000(this Int32 number)
+        {
+            return number * 1000;
+        }
+    }
+}
---
+++ swapGenericList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swapGenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tal = new List<int>(5);
+            Debug.WriteLine(tal);
+            Console.ReadKey();
+        }
+    }
+    public class GenericUtl
+    {
+//        public List<T> reversList<T>()
+        public void reversList<T>(List<T> oldlist)
+        {
+            List<T> svar = new List<T>();
+            //List<T> input = this.MemberwiseClone();
+            //while (this.reversList)
+            foreach (T item in oldlist)
+            {
+                svar.Insert(0,item);
+            }
+                       
+            //return svar;
+        }
+    }
+}
