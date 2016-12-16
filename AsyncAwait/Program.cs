d5fde3c69be6fa09371fcce395694f0b8613c8b5
Stef-R <username@user.noreply.github.com>
2016-12-16 15:58:04 +0100

    Push state

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using AsyncAwait;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MainAsync mitTask = new AsyncAwait.MainAsync();
+            MainAsync().Wait();
+            //MainAsync();  // what happens if we don't wait?
+        }
+        public static async void MainAsync() 
+        {
+            Console.WriteLine("Call MailAsync");
+            await Task.Delay(2000);
+            //Task.Delay(2000);  // what happens if we don't await?
+            //return ;
+        }
+    }
+
+}
